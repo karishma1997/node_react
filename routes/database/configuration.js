82a946e0269fc74b1dc00e6aabdf232fafbee0e5
emachalanema <emachalanema@gmail.com>
2021-11-06 15:21:37 +0530

    minor

--- routes/database/configuration.js
+++ routes/database/configuration.js
@@ -1,8 +1,8 @@
 const { Pool, Client } = require('pg')
 const dotenv = require('dotenv').config();
-const connectString = process.env.DATABASE_URL;
+// const connectString = process.env.DATABASE_URL;
 
-console.log("connection string", connectString);
+// console.log("connection string", connectString);
 const client = new Client({
     user: process.env.PGUSER,
     password: process.env.PGPASSWORD,
@@ -13,17 +13,18 @@ const client = new Client({
     idleTimeoutMillis: 0,
   connectionTimeoutMillis: 0,
 })
-async function createUser() {
+client.connect();
+
+function createUser() {
     try {
-        client.connect();
-        var userTable = "create table [if not exists] users(id int primary key not null,name text not null, email char(50) not null, phone_number bigint not null, address char(100) not null)";
+        var userTable = "create table if not exists users (id int primary key not null,name text not null, email char(50) not null, phone_number bigint not null, address char(100) not null)";
         client.query(userTable, function (err, result1) {
             if (err) {
                 console.log("create table usererror",err);
             }
             else {
                 console.log("create table user success",result1);
-                var postsTable = "create table [if not exists] posts(id int primary key not null, text text not null, description text not null, user_id int not null,foreign key(user_id) references user(id)";
+                var postsTable = "create table if not exists posts (id int primary key not null, text text not null, description text not null, user_id int not null, foreign key (user_id) references users(id))";
                 client.query(postsTable, function (err, result) {
                     if (err) {
                         console.log("create table posts",err);
--- routes/index.js
+++ routes/index.js
@@ -2,64 +2,56 @@ const client = require('./database/configuration')
 var express = require('express');
 var router = express.Router();
 
-router.post('/posts', function(req,res){
-  user_id=req.body.user_id;
-  id=req.body.id;
-  text=req.body.text;
-  description=req.body.description;
+router.post('/posts', function (req, res) {
+  user_id = req.body.user_id;
+  id = req.body.id;
+  text = req.body.text;
+  description = req.body.description;
   var query = "Insert into posts (user_id,id,text,description) values (?,?,?,?,?)";
-client.query(query,[user_id,id,text,description], function(err,result){
-  if(err)
-  {
-    res.send(err);
-  }
-  else
-  {
-    res.send(result);
-  }
+  client.query(query, [user_id, id, text, description], function (err, result) {
+    if (err) {
+      res.send(err);
+    }
+    else {
+      res.send(result);
+    }
+  })
 })
-})
-router.get('/posts/:id', function(req,res){
+router.get('/posts/:id', function (req, res) {
   id = req.params.id;
   var query = " select * from posts where id=?";
-  client.query(query,[id],function(err,result){
-if(err)
-{
-  res.send(err);
-}
-else
-{
-  res.send(result);
-}
+  client.query(query, [id], function (err, result) {
+    if (err) {
+      res.send(err);
+    }
+    else {
+      res.send(result);
+    }
+  })
 })
-})
-router.get('/posts/userid/:user_id', function(req,res){
+router.get('/posts/userid/:user_id', function (req, res) {
   user_id = req.params.user_id;
   var query = " select * from posts where id=?";
-  client.query(query,[user_id],function(err,result){
-if(err)
-{
-  res.send(err);
-}
-else
-{
-  res.send(result);
-}
+  client.query(query, [user_id], function (err, result) {
+    if (err) {
+      res.send(err);
+    }
+    else {
+      res.send(result);
+    }
+  })
 })
-})
-router.get('/users/posts/:user_id', function(req,res){
-user_id = req.params.user_id;
-var query ="select users.name, users.email, users.password, users.phone_number, posts.text, posts.description from users full outer join posts on users.id=posts.user_id";
-client.query(query,[user_id], function(err,result){
-  if(err)
-  {
-    res.send(err);
-  }
-  else
-  {
-    res.send(result);
-  }
-})
+router.get('/users/posts/:user_id', function (req, res) {
+  user_id = req.params.user_id;
+  var query = "select users.name, users.email, users.password, users.phone_number, posts.text, posts.description from users full outer join posts on users.id=posts.user_id";
+  client.query(query, [user_id], function (err, result) {
+    if (err) {
+      res.send(err);
+    }
+    else {
+      res.send(result);
+    }
+  })
 })
 
 
