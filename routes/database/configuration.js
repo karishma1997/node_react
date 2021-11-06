52654c7ac636ea03857fb7cf3ac04a25ba32e03a
arunkumar.sak19143 <arunkumar.sak19143@gmail.com>
2021-11-06 14:01:55 +0530

    'minor'

--- routes/database/configuration.js
+++ routes/database/configuration.js
@@ -6,22 +6,23 @@ console.log("connection string", connectString);
 const client = new Client({
     connectString
 })
-client.connect()
 async function createUser() {
     try {
+        client.connect();
         var userTable = "create table [if not exists] users(id int primary key not null,name text not null, email char(50) not null, phone_number bigint not null, address char(100) not null)";
-        client.query(userTable, function (err, result) {
+        client.query(userTable, function (err, result1) {
             if (err) {
-                console.log(err);
+                console.log("create table usererror",err);
             }
             else {
+                console.log("create table user success",result1);
                 var postsTable = "create table [if not exists] posts(id int primary key not null, text text not null, description text not null, user_id int not null,foreign key(user_id) references user(id)";
                 client.query(postsTable, function (err, result) {
                     if (err) {
-                        console.log(err);
+                        console.log("create table posts",err);
                     }
                     else {
-                        console.log(result);
+                        console.log("create table posts success",result)
                     }
                 })
             }
