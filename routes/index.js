1a03723fe333f3f3edb68291ff96c453369a5694
arunkumar.sak19143 <arunkumar.sak19143@gmail.com>
2021-11-06 17:13:47 +0530

    commit

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-const client = require('./database/configuration')
+const {client} = require('./database/configuration')
 var express = require('express');
 var router = express.Router();
 
@@ -7,19 +7,19 @@ router.post('/posts', function (req, res) {
   id = req.body.id;
   text = req.body.text;
   description = req.body.description;
-  var query = "Insert into posts (user_id,id,text,description) values (?,?,?,?,?)";
+  var query = "Insert into posts (user_id,id,text,description) values ($1,$2,$3,$4) returning text,description";
   client.query(query, [user_id, id, text, description], function (err, result) {
     if (err) {
       res.send(err);
     }
     else {
-      res.send(result);
+      res.send(result.rows[0]);
     }
   })
 })
 router.get('/posts/:id', function (req, res) {
   id = req.params.id;
-  var query = " select * from posts where id=?";
+  var query = "select * from posts where id=$1";
   client.query(query, [id], function (err, result) {
     if (err) {
       res.send(err);
@@ -31,19 +31,19 @@ router.get('/posts/:id', function (req, res) {
 })
 router.get('/posts/userid/:user_id', function (req, res) {
   user_id = req.params.user_id;
-  var query = " select * from posts where id=?";
+  var query = " select * from posts where user_id=$1";
   client.query(query, [user_id], function (err, result) {
     if (err) {
       res.send(err);
     }
     else {
-      res.send(result);
+      res.send(result.rows);
     }
   })
 })
 router.get('/users/posts/:user_id', function (req, res) {
   user_id = req.params.user_id;
-  var query = "select users.name, users.email, users.password, users.phone_number, posts.text, posts.description from users full outer join posts on users.id=posts.user_id";
+  var query = "select *users, *posts from users inner join posts on users.id=$1";
   client.query(query, [user_id], function (err, result) {
     if (err) {
       res.send(err);
